2b4d6441a66756979ceeb25b0fca696b19cc2522
daikiminami <rondaiten127@gmail.com>
2019-05-24 10:08:15 +0800

    fixed learned

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
     if params[:status] == nil
       @categories = Category.joins(:words).group("categories.id")
     elsif params[:status]=='learned'
-      @categories = Category.joins(:lessons).group("categories.id").where(lessons: {user_id: current_user.id}).where.not(lessons: {result: "nil"})
+      @categories = Category.joins(:lessons).group("categories.id").where(lessons: {user_id: current_user.id}).where.not(lessons: {result: "nil"}).joins(:words).group(:id)
     else params[:status]=='unlearned'
       @categories = Category.joins("LEFT OUTER JOIN lessons ON categories.id = lessons.category_id AND lessons.user_id = #{current_user.id}").where(lessons: {result: nil}).joins(:words).group(:id)
     end
